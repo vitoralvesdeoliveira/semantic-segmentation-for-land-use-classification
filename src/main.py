8161b1ee1691ee84edbeb0efb0bfc1d3c95b58a8
vitor <videoliva@gmail.com>
2025-09-14 13:13:34 -0300

    altera nome de arquivo de dataset .npz gerado

--- src/main.py
+++ src/main.py
@@ -26,6 +26,6 @@ extrator.extrair_lotes(fids_a_processar=[0,1,2,3,4,5,6,7,8,9,10,22,24,25,35,36,1
 
 run_preprocessing_pipeline(
     PROCESSED_DATA,
-    os.path.abspath(os.path.join(PROCESSED_DATA)),
+    os.path.abspath(os.path.join(PROCESSED_DATA,'dataset-images-labels')),
     256,
     COLOR_MAP)
