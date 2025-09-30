811eaec5911c0765ad1daeea125827c3fe8e7388
Vítor Alves <videoliva@gmail.com>
2025-09-30 10:25:22 -0300

    Update keras.py

--- src/utils/keras.py
+++ src/utils/keras.py
@@ -3,3 +3,7 @@ import keras
 
 def load_model(path : str):
     return keras.models.load_model(os.path.abspath(path))
+
+def predict(model_path : str, img_path: str ):
+    model = load_model(path)
+    return keras.models.load_model(os.path.abspath(path))
